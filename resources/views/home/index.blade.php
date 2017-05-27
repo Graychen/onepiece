54f7520a41e380d82d33ae3248bf619f5df2aad1
Graychen <13780185250@sina.cn>
2017-05-27 23:04:43 +0800

    添加首页样式

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,15 @@
-        @foreach ($goods as $good)
-            {{ $good->name }}
-            {{ $good->pic }}
-        @endforeach
+@extends('layouts.home')
+
+@section('content')
+@foreach ($goods as $good)
+<div class="row">
+  <div class="col-xs-6 col-md-3">
+      <a href="#" class="thumbnail">
+            <img src="{{ $good->pic }}" alt="{{ $good->name }}">
+      </a>
+  </div>
+</div>
+    
+@endforeach
+
+@endsection
