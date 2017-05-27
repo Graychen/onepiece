b4e7fd94280ad391812c864fdc7a810a5f26344b
Graychen <13780185250@sina.cn>
2017-05-28 00:01:50 +0800

    添加提交照片的样式

--- resources/views/home/create.blade.php
+++ resources/views/home/create.blade.php
@@ -1,10 +1,10 @@
-<html>
-<body>
+@extends('layouts.home')
 
+@section('content')
 <form method="post" action="{{url('upload')}}"
 enctype="multipart/form-data">
 <input type="hidden" name="_token" value="{{ csrf_token()  }}">
-<label for="file">Filename:</label>
+<label for="file">商品图片:</label>
 <input type="file" name="file" id="file" /> 
 <label for="file">商品名称:</label>
 <input type="input" name="name" id="name" /> 
@@ -12,5 +12,5 @@ enctype="multipart/form-data">
 <input type="submit" name="submit" value="Submit" />
 </form>
 
-</body>
-</html>
+
+@endsection
