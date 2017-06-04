94897bc93d00be75cf8e504479949be180d44f27
Graychen <13780185250@sina.cn>
2017-06-04 14:58:41 +0800

    添加发送邮件的测试

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Mail;
+
+class MailController extends Controller
+{
+       public function send() 
+       {
+               $name="graychen";
+               $flag= Mail::send('emails.test',['name'=>$name],function($message){
+                       $to='13780185250@163.com';
+                       $message->to($to)->subject('测试邮件');
+               });
+               var_dump($flag);
+               if($flag){
+                       echo '发送成功,请查收!';
+               }else{
+                       echo '发送失败,请重试!';
+               }
+       }
+}
