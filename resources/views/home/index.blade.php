032fd4d9b40c05e431275ea0b56833c670f9f1f3
Graychen <13780185250@sina.cn>
2017-06-05 23:46:07 +0800

    fix(index): 首页样式调整

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
 @foreach ($goods as $good)
-<div class="row">
   <div class="col-xs-6 col-md-3">
       <a href="#" class="thumbnail">
             <img src="{{Storage::url($good->pic)}}" alt="{{ $good->name }}">
+                  <h3>{{ $good->name }}</h3>
       </a>
   </div>
-</div>
     
 @endforeach
 
